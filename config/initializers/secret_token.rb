13d7b9d272195e75750acc58517421c1dbfd4c39
Andre Quina <andre.quina@gmail.com>
2012-03-17 17:28:50 -0400

    initial add of rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bonnie::Application.config.secret_token = '780e11b4bed90fd821144db20092b3c418617de8c55120077a937d2d5ef2fc937133758f1cc0db6243d278af9428fa81a4edcbb0b52f394bb876ba4aececca47'
